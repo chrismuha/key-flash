d8be2d5d995a7a646ebb4ab96e5b6f4b6ab7b50d
muhachris297 <muhachris297@gmail.com>
2025-02-15 22:55:46 -0500

    fixed rectangle one not appearing.

--- website-practice/navbar-vertical/js/navbar-vertical.js
+++ website-practice/navbar-vertical/js/navbar-vertical.js
@@ -1,6 +1,15 @@
 function toggleNavbar() {
     const navbar = document.querySelector('.navbar');
     const content = document.querySelector('.content');
+    const rectangle = document.querySelector('#rectangle1');
+
     navbar.classList.toggle('active');
     content.classList.toggle('shifted');
+
+    // Toggle visibility of #rectangle1
+    if (navbar.classList.contains('active')) {
+        rectangle.style.display = "block";
+    } else {
+        rectangle.style.display = "none";
+    }
 }
