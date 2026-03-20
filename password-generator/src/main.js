02b24c954eddeee03e5f898ba771934cb0a77017
muhachris297 <muhachris297@gmail.com>
2026-03-19 22:36:31 -0400

    fixed electron in all repos.

--- password-generator/src/main.js
+++ password-generator/src/main.js
@@ -1,5 +1,37 @@
-import { createApp } from 'vue';
-import App from './App.vue';
-import './style.css';
+const { app, BrowserWindow } = require('electron');
+const path = require('path');
 
-createApp(App).mount('#app');
+let mainWindow;
+
+function createWindow() {
+  mainWindow = new BrowserWindow({
+    width: 1500,
+    height: 1080,
+    minWidth: 1180,
+    minHeight: 860,
+    backgroundColor: '#111111',
+    title: 'Password Generator',
+    titleBarStyle: process.platform === 'darwin' ? 'hiddenInset' : 'default',
+    webPreferences: {
+      preload: path.join(__dirname, 'preload.js'),
+      contextIsolation: true,
+      nodeIntegration: false
+    }
+  });
+
+  if (process.env.NODE_ENV === 'development') {
+    mainWindow.loadURL('http://localhost:5174');
+  } else {
+    mainWindow.loadFile(path.join(__dirname, '../dist/index.html'));
+  }
+}
+
+app.whenReady().then(createWindow);
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') app.quit();
+});
+
+app.on('activate', () => {
+  if (BrowserWindow.getAllWindows().length === 0) createWindow();
+});
