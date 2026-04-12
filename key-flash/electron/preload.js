a754811c99fe1b6e41d9b28c412205bd04fd325e
muhachris297 <muhachris297@gmail.com>
2026-04-12 12:51:28 -0400

    Remove key-flash after repo split

--- key-flash/electron/preload.js
+++
@@ -1,8 +0,0 @@
-const { contextBridge, ipcRenderer } = require('electron');
-
-contextBridge.exposeInMainWorld('keyFlashAPI', {
-  getSettings: () => ipcRenderer.invoke('settings:get'),
-  setSettings: (patch) => ipcRenderer.invoke('settings:set', patch),
-  setFullscreen: (value) => ipcRenderer.invoke('window:setFullscreen', value),
-  toggleFullscreen: () => ipcRenderer.invoke('window:toggleFullscreen')
-});
